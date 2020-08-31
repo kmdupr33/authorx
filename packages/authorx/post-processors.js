b07c26e49a90daf700a2db035966f33ab51f99b3
Matt Dupree <kmdupr33@gmail.com>
2020-08-31 08:43:53 -0400

    remove unneeded post processing

--- packages/authorx/post-processors.js
+++ packages/authorx/post-processors.js
@@ -1,23 +1,5 @@
-const getChildren = (authorx) => {
-  const children = [...authorx.flat().filter((el) => el)];
-  if (!children.every(({ type }) => type === "text" || type === "whitespace")) {
-    return children;
-  }
-  return [
-    children.reduce(
-      (acc, child) => {
-        return {
-          type: "text",
-          value: acc.value + child.value,
-        };
-      },
-      { value: "" }
-    ),
-  ];
-};
-
 const postProcessAuthorX = ([_, invocation, _1, _2, _3, authorx]) => {
-  const children = getChildren(authorx);
+  const children = [...authorx.flat().filter((el) => el)];
   return {
     type: "functionInvocation",
     ...invocation,
