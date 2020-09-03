5b917479692e025d036636b38551b40454170731
Matt Dupree <kmdupr33@gmail.com>
2020-09-03 09:42:07 -0400

    add comment function; update readme with better quickstart section

--- packages/x-faux-markdown/index.js
+++ packages/x-faux-markdown/index.js
@@ -7,4 +7,6 @@ module.exports = {
   a: (string, [url]) => `[${string.trim()}](${url.value})`,
   ">": (string, lang) =>
     `\`\`\`${(lang && lang[0].value) || ""}${string}\`\`\``,
+  // A comment function
+  "//": () => "",
 };
