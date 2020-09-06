b1d57562274aca2797120972c5fb391aed5927ef
Matt Dupree <kmdupr33@gmail.com>
2020-09-06 10:22:24 -0400

    better cli ux

--- packages/compiler/bin/compile.js
+++ packages/compiler/bin/compile.js
@@ -5,12 +5,33 @@ const { createParser } = require("../lib/parser");
 const { compile, Scope } = require("../lib/code-gen");
 
 const parser = createParser();
-const [_, _1, axFilePath, funcMapPath] = process.argv;
-const ast = parser.parse(fs.readFileSync(axFilePath, { encoding: "utf8" }))[0];
-const funcMap = require(path.resolve(process.cwd(), funcMapPath));
-const scope = new Scope();
-scope.push(funcMap());
-(async () => {
-  const outputDir = path.dirname(axFilePath);
-  console.log(await compile(ast, scope, outputDir));
-})();
+require("yargs")
+  .scriptName("axc")
+  .usage(
+    "$0 <axFilePath> <functionsFilePath>",
+    "compile an authorx file",
+    (yargs) => {
+      yargs.positional("axFilePath", {
+        type: "string",
+        describe: "the authorx file you want to compile",
+      });
+      yargs.positional("functionsFilePath", {
+        type: "string",
+        describe:
+          "the node module the exports the functions you want to use in your authorx file",
+      });
+    },
+    ({ axFilePath, functionsFilePath }) => {
+      const ast = parser.parse(
+        fs.readFileSync(axFilePath, { encoding: "utf8" })
+      )[0];
+      const funcMap = require(path.resolve(process.cwd(), functionsFilePath));
+      const scope = new Scope();
+      scope.push(funcMap());
+      (async () => {
+        const outputDir = path.dirname(axFilePath);
+        console.log(await compile(ast, scope, outputDir));
+      })();
+    }
+  )
+  .help().argv;
